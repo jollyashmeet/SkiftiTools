a372434658329323a0aee203205c442d85396a7e
jollyashmeet <96188204+jollyashmeet@users.noreply.github.com>
2025-06-01 18:46:35 +0300

    Changing docker version number

--- build_R_skiftiTools.sh
+++ build_R_skiftiTools.sh
@@ -5,5 +5,5 @@ cp ../git/skiftiTools/R/Skifti_view.R .
 cp ../git/skiftiTools/R/Skifti2Nifti.R .
 cp ../git/skiftiTools/R/Skifti_read.R .
 cp ../R/Postprocess.R .
-echo docker build . --no-cache -f Dockerfile.txt -t ashjoll/skiftitools:0.1.1 &> build_R.log
-docker build . -f Dockerfile.txt -t ashjoll/skiftitools:0.1.1
+echo docker build . --no-cache -f Dockerfile.txt -t ashjoll/skiftitools:0.2.0 &> build_R.log
+docker build . -f Dockerfile.txt -t ashjoll/skiftitools:0.2.0
