c3f3051e77c3b90186af6d1d15342787817482f1
jollyashmeet <96188204+jollyashmeet@users.noreply.github.com>
2025-06-01 19:08:55 +0300

    Update build_R_skiftiTools.sh

--- build_R_skiftiTools.sh
+++ build_R_skiftiTools.sh
@@ -1,9 +1,9 @@
-cp ../git/skiftiTools/R/Nifti2Skifti.R .
-cp ../git/skiftiTools/R/writeSkifti.R .
-cp ../git/skiftiTools/R/writeCSV.R .
-cp ../git/skiftiTools/R/Skifti_view.R .
-cp ../git/skiftiTools/R/Skifti2Nifti.R .
-cp ../git/skiftiTools/R/Skifti_read.R .
-cp ../R/Postprocess.R .
+curl -O https://raw.githubusercontent.com/haanme/skiftiTools/main/R/Nifti2Skifti.R
+curl -O https://raw.githubusercontent.com/haanme/skiftiTools/main/R/writeSkifti.R
+curl -O https://raw.githubusercontent.com/haanme/skiftiTools/main/R/writeCSV.R
+curl -O https://raw.githubusercontent.com/haanme/skiftiTools/main/R/Skifti_view.R
+curl -O https://raw.githubusercontent.com/haanme/skiftiTools/main/R/Skifti2Nifti.R
+curl -O https://raw.githubusercontent.com/haanme/skiftiTools/main/R/Skifti_read.R
+curl -O https://raw.githubusercontent.com/jollyashmeet/SkiftiTools/main/Postprocess.R
 echo docker build . --no-cache -f Dockerfile.txt -t ashjoll/skiftitools:0.2.0 &> build_R.log
 docker build . -f Dockerfile.txt -t ashjoll/skiftitools:0.2.0
