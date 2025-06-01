3b5786a9b4aa09e4200864563204e2470870cd90
jollyashmeet <96188204+jollyashmeet@users.noreply.github.com>
2025-06-01 19:28:40 +0300

    Update build_R_skiftiTools.sh

--- build_R_skiftiTools.sh
+++ build_R_skiftiTools.sh
@@ -1,9 +1,18 @@
+# Download all necessary R scripts from haanme's repo
 curl -O https://raw.githubusercontent.com/haanme/skiftiTools/main/R/Nifti2Skifti.R
 curl -O https://raw.githubusercontent.com/haanme/skiftiTools/main/R/writeSkifti.R
 curl -O https://raw.githubusercontent.com/haanme/skiftiTools/main/R/writeCSV.R
 curl -O https://raw.githubusercontent.com/haanme/skiftiTools/main/R/Skifti_view.R
 curl -O https://raw.githubusercontent.com/haanme/skiftiTools/main/R/Skifti2Nifti.R
 curl -O https://raw.githubusercontent.com/haanme/skiftiTools/main/R/Skifti_read.R
+
+## Download all necessary R scripts from jollyashmeet's repo
 curl -O https://raw.githubusercontent.com/jollyashmeet/SkiftiTools/main/Postprocess.R
+
+# Make enigmaDTI folder and download the skeleton mask
+mkdir -p enigmaDTI
+curl -o enigmaDTI/ENIGMA_DTI_FA_skeleton_mask.nii.gz https://raw.githubusercontent.com/jollyashmeet/SkiftiTools/main/enigmaDTI/ENIGMA_DTI_FA_skeleton_mask.nii.gz
+
+#Build Docker image
 echo docker build . --no-cache -f Dockerfile.txt -t ashjoll/skiftitools:0.2.0 &> build_R.log
 docker build . -f Dockerfile.txt -t ashjoll/skiftitools:0.2.0
